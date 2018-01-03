0b31ff3bd6611b7948764ecae6817e2b4ac3eb22
Christopher Dorst <chris@dorst.email>
2018-01-03 04:54:07 -0600

    Add GitHubRepositoryCreator

---
+++ DevOps.VersionControl.Functions.CreateGitHubRepository/GitHubRepositoryCreator.cs
@@ -0,0 +1,16 @@
+﻿using Common.Structures.HttpBasicAuthentication;
+using System.Threading.Tasks;
+using static Common.Functions.PostJsonWithBasicAuthentication.BasicAuthJsonApiPoster;
+using static DevOps.VersionControl.Functions.CreateGitHubRepository.CreateRepositoryRequestHelper;
+using static Metaproject.Users.CDorst.GitHubAccessToken.GitHubCredentials;
+
+namespace DevOps.VersionControl.Functions.CreateGitHubRepository
+{
+    public static class GitHubRepositoryCreator
+    {
+        public static async Task Create(string name, string description, BasicAuthenticationCredentials? credentials = null)
+            => await Post(
+                CreateRepository(name, description),
+                credentials ?? CDorst);
+    }
+}
