26393df9f5f536d8d580c681190fc081ce35a77e
Christopher Dorst <chris@dorst.email>
2018-01-03 04:54:01 -0600

    Add request JSON helper

---
+++ DevOps.VersionControl.Functions.CreateGitHubRepository/CreateRepositoryRequestHelper.cs
@@ -0,0 +1,16 @@
+﻿using Common.Structures.HttpJsonRequest;
+
+namespace DevOps.VersionControl.Functions.CreateGitHubRepository
+{
+    internal static class CreateRepositoryRequestHelper
+    {
+        private const string BaseAddress = "https://api.github.com";
+        private const string Path = "/user/repos";
+
+        public static ApiRequest CreateRepository(string name, string description)
+            => new PostRequest(BaseAddress, Path, GetJson(name, description));
+
+        private static string GetJson(string name, string description)
+            => $"{{ \"name\": \"{name}\", \"description\": \"{description}\" }}";
+    }
+}
